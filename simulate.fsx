51e8c80817102117879aaab94503e8409802a9bd
MadsFrost <madsanton31@gmail.com>
2020-12-01 11:37:03 +0100

    DRONER

--- simulate.fsx
+++ simulate.fsx
@@ -1,47 +1,64 @@
 type vec = float*float
 
-//Scale a vector A by a length B
-let (+*) (A:vec) (B:float) : vec =
-    (fst A * B, snd A * B)
+let origin : vec = (0.,0.)
+let upwards : vec = (0.,1.)
+let right : vec = (1.,0.)
+let downwards : vec = (0.,-1.)
+let left : vec = (-1.,0.)
 
-//Add two vectors A & B
-let (++) (A:vec) (B:vec) : vec =
+// Scale the vector with a float
+let (+@) (A:vec) (B:float) : vec =
+    (fst A*B, snd A*B)
+
+// Add two vectors together
+let (+|) (A:vec) (B:vec) : vec = 
     (fst A + fst B, snd A + snd B)
 
-//Subtract two vectors A & B
+// Subtract two vectors from eachother
 let (+-) (A:vec) (B:vec) : vec =
     (fst A - fst B, snd A - snd B)
 
-//Get length of a vector
-let length (A:vec) : float =
-    sqrt (fst A**2. + snd A**2.)
+let vectorLength (A:vec) : float =
+    sqrt(fst A**2. + snd A**2.)
 
-//Get vector in same direction but with length 1
-let normalize (A:vec) : vec =
-    let len = length A
-    (fst A / len, snd A / len)
+let normVector (A:vec) : vec =
+    let length = vectorLength A 
+    (fst A / length, snd A / length)
 
-let start : vec = (0.,2.)
-let slutt : vec = (3.,3.)
-let speed = 2.5
-// k^2 + k^2 = h^2 
-
-
-let path = slutt +- start  
-let lengde = sqrt((fst path)**2. + (snd path)**2.) 
-
-let normPath = (normalize(path))
-let newPath = (normPath +* speed)
-
-let cordNewPath = (start ++ newPath)
-
-printfn "%A %A" start slutt
-printfn "%A" newPath
-printfn "%A" cordNewPath
+let testVector : vec = (0.,1.)
 
 
 
+type Drone (startpos:vec, dest:vec, speed:float, UUID:string) = 
+    let mutable position = (startpos)
+    let mutable destination = (dest)
+    let mutable speed = speed
+    let mutable UUID = "Package - " + UUID
 
-printfn "%A" (normalize(path))
-printfn "%A" (length(normalize(path)))
-printfn "%A" (length(normalize(path)+*2.5))
+
+    member this.NewPosition () = (normVector(dest +- position) +@ speed)
+    member this.Fly () = 
+    if (vectorLength(dest +- position) <= speed) then
+        printfn "%s %s %A" UUID "- Has overshot and flown back to its destination:" destination
+        position <- dest
+        ()
+    else
+        if(vectorLength(dest +- position) > speed) then 
+            printfn "%s %s %A" UUID "- Has reached its destination. " destination
+            position <- dest
+            ()
+        else 
+            printfn "%s %s %A" UUID "- Is still flying at position: " position
+            position <- position +| this.NewPosition(); position
+            ()
+
+type Airspace () = 
+    let mutable drones : Drone list = []
+
+    member this.droneDist = 
+    member this.flyDrones = 
+    member this.addDrone = 
+    member this.willCollide = 
+
+let drone1 = new Drone((upwards +@ 15.), (origin), 16., "Queen-sized bed")
+drone1.Fly()
